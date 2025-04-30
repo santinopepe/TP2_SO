035d11489717933be305aa154bcf7889e6ef5ab6
snogueira1 <snogueira@itba.edu.ar>
2025-04-30 16:20:11 -0300

    Cosas varias

---
+++ Kernel/include/MemoryManager.h
@@ -0,0 +1,16 @@
+#ifndef MEMORY_MANAGER_H
+#define MEMORY_MANAGER_H
+
+#include <stdlib.h>
+
+typedef struct MemoryManagerCDT *MemoryManagerADT;
+
+MemoryManagerADT createMemoryManager(void *const restrict memoryForMemoryManager, void *const restrict managedMemory);
+
+void * allocMemory(MemoryManagerADT const restrict memoryManager, const size_t memoryToAllocate);
+
+void free(void * memoryToFree); 
+
+void getMemoryInfo(MemInfo * mem); 
+
+#endif
