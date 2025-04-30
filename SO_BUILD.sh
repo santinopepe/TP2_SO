6f759e311de7bd194d5bc49692da4408039859fe
santinopepe <spepe@itba.edu.ar>
2025-04-29 22:20:21 -0300

    Agregado de script de ejecucion

---
+++ SO_BUILD.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e  # Termina el script si algún comando falla
+
+# Imagen de Docker
+IMAGE_NAME="agodio/itba-so-multi-platform:3.0"
+PROJECT_PATH="/root"
+SKIP_RUN=false
+
+# Verificar argumentos
+for arg in "$@"; do
+  if [[ $arg == "--no-run" ]]; then
+    SKIP_RUN=true
+  fi
+done
+
+
+docker run --rm -v "${PWD}:/root" --privileged -ti "$IMAGE_NAME" bash -c "
+    make clean -C ${PROJECT_PATH}/Toolchain
+    make clean -C ${PROJECT_PATH}
+    make -C ${PROJECT_PATH}/Toolchain
+    make -C ${PROJECT_PATH}
+"
+
+
+if [ "$SKIP_RUN" = false ]; then
+  ./run.sh
+else
+  echo " Ejecución de run.sh omitida (--no-run)"
+fi
+
