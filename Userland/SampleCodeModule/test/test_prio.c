a95ab90795987d30700f4213551451812270eaf4
santinopepe <spepe@itba.edu.ar>
2025-06-02 17:31:16 -0300

    agregado de printf
    Co-Authored-By: Manuel Araujo Feltrin <152537869+maraujofeltrin@users.noreply.github.com>
    Co-Authored-By: Santiago Nogueira <147627792+snogueira1@users.noreply.github.com>

--- Userland/SampleCodeModule/test/test_prio.c
+++ Userland/SampleCodeModule/test/test_prio.c
@@ -3,8 +3,8 @@
 #include <syscalls.h>
 #include <test_util.h>
 
-#define MINOR_WAIT 1000000 // TODO: Change this value to prevent a process from flooding the screen
-#define WAIT 10000000      // TODO: Change this value to make the wait long enough to see theese processes beeing run at least twice
+#define MINOR_WAIT 1000000 
+#define WAIT 10000000      
 
 #define TOTAL_PROCESSES 3
 #define LOWEST 0  // TODO: Change as required
